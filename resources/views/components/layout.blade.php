2b3e238132128102d77caf3b336c9eec55abc10a
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-12-15 21:14:33 +0700

    CRUD BLOG 1

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pathnity</title>
+    @vite('resources/css/app.css', 'resources/js/app.js') 
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+</head>
+<body class=" h-screen flex flex-col">
+    <x-navbar></x-navbar>
+    <main class="bg-[#F7F7F7]">
+        {{$slot}}
+    </main>
+    <x-footer></x-footer>
+
+    <script src="{{ asset('js/navbar.js') }}"></script>
+
+    
+
+</body>
+</html>
