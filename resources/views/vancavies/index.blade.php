61f588c7b6c1fe86156b75aebd8988c3feb9450f
Annisaseptiyani <annisa.223040172@mail.unpas.ac.id>
2024-12-25 17:06:14 +0700

    index blade update

--- resources/views/vancavies/index.blade.php
+++ resources/views/vancavies/index.blade.php
@@ -3,40 +3,22 @@
 @section('content')
 <div class="container">
     <h1>Daftar Vancavies</h1>
-    <a href="{{ route ('vancavies.create')}}" class="btn-primary">Tambah Data</a>
-    <table class="table mt-4">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Title</th>
-                <th>Content</th>
-                <th>Image</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($vancavies as $item)
-                <tr>
-                    <td>{{ $item->title}}</td>
-                    <td>{{ $item->content }}</td>
-                    <td>
-                        @if($item->image)
-                            <img src="{{ asset('storage/' . $item->image)}}"width="50">
-                        @endif
-                    </td>
-                    <td>
-                        <a href="{{ route('vancavies.show',$item->id) }}" class="btn btn-info btn-sm">Detail</a>
-                        <a href="{{ route('vancavies.edit',$item->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                        <form action="{{ route('vancavies.destroy',$item->id) }}" method="POST" style="display:inline-block;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure?')">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-                </tbody>
-    </table>
-</div>
-
-@endsection
+    <a href="{{ route ('vancavies.create')}}" class="btn-primary">Create New Vancavy</a>
+    <ul>
+        @foreach ($vancavies as $vancavy)
+        <li>
+            <h2>{{ $vancavy->title }}</h2>
+            <p>{{ $vancavy->content }}</p>
+            @if($vancavy->image)
+                <img src="{{ asset('storage/'. $vancavy->image)}}"width="200">
+            @endif
+            <a href="{{ route('vancavies.edit', $vancavy) }}">Edit</a>
+                <form action="{{ route('vancavies.destroy', $vancavy) }}" method="POST" onsubmit="return confirm('Are you sure?')">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Delete</button>
+                </form>
+        </li>
+        @endforeach
+    </ul>
+    @endsection
