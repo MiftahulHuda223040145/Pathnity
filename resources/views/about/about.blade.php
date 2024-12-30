ba975bdac6a056a3c73bb42106337c3917c9ac7b
sena606 <gentasn@gmail.com>
2024-12-30 22:38:40 +0700

    update slicing about us

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,18 +1,24 @@
 <x-layout>
     <div class="container mx-auto p-6 mt-28">
         <div class="bg-white shadow-md rounded-md overflow-hidden">
-            <!-- First Detail -->
-                <div class="items-center space-x-4">
-                    <div class="w-16 h-16 rounded-full overflow-hidden">
-                       
-                    </div>
-                        <h3 class="font-semibold">Pathinity adalah platform
-                            penghubung untuk talent dengan organisasi atau perusahaan.</h3>
-                </div>
-            <!-- Background Image -->
-            <div class="w-full h-60 bg-cover bg-center" style="background-image: url('{{ asset('img/background/background.jpeg') }}');">
+            <div class="text-left p-6">
+                <h1 class="font-semibold text-3xl">ABOUT PATHNITY</h1>
+                <h3 class="font-semibold text-1xl mt-4">
+                    Pathinity adalah platform penghubung untuk talent dengan organisasi atau perusahaan.
+                </h3>
+                <h3 class="font-semibold text-3xl mt-6">VISION AND MISSION</h3>
+                <h3 class="font-semibold text-1xl mt-4">
+                    Menjadi platform terpercaya yang menghubungkan individu dengan peluang karier dan kegiatan sukarela, guna menciptakan dampak positif bagi komunitas dan dunia kerja.
+                </h3>
+                <ol class="font-semibold list-decimal list-inside mt-4">
+                    <li>Menyediakan akses mudah dan transparan ke berbagai lowongan pekerjaan dan kegiatan sukarela dari berbagai sektor.</li>
+                    <li>Mendorong pengembangan diri melalui peluang relawan yang bermakna dan relevan.</li>
+                    <li>Mendukung perusahaan dan organisasi sosial dalam menemukan kandidat yang sesuai dengan kebutuhan mereka.</li>
+                    <li>Memberikan fitur pencarian dan filter canggih untuk mempermudah pengguna menemukan peluang yang sesuai dengan minat dan keterampilan mereka.</li>
+                    <li>Membantu pengguna membangun portofolio profesional yang mencerminkan pengalaman kerja dan kontribusi sukarela mereka.</li>
+                    <li>Meningkatkan kesadaran tentang pentingnya kontribusi sosial melalui kegiatan relawan.</li>
+                </ol>
             </div>
-        </div>  
         </div>
     </div>
 </x-layout>
